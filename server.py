b26dbbfdc9c6f28ca02fc70c29649300ba12d6d6
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-11-03 15:18:32 -0600

    Data Users to Browser

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
-from flask import Flask
-from flask.templating import render_template
+from flask import Flask, render_template, redirect
+from model_users import User
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("read(all).html")
+    return redirect('/users')
+
+@app.route('/users')
+def users():
+    return render_template("read(all).html", users=User.get_all())
 
 if __name__ == "__main__":
     app.run(debug = True)
